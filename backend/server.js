fd992b4b9d4a775a4044b5d12dc741ac8d216511
smurki0 <123moibrahem123@gmail.com>
2026-03-02 19:29:07 +0200

    Add console log for server startup

--- backend/server.js
+++ backend/server.js
@@ -129,7 +129,9 @@ app.delete("/api/orders/:id", (req, res) => {
 // ================== RUN ==================
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 
 app.use("/Asstar", express.static("Asstar"));
 
